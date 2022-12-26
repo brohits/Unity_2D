b6d1dfa18a29bf3a6c5d4eb10f71cf435c631c35
hardik <harrrdik18@gmail.com>
2022-12-26 15:52:16 +0530

    added jumping

--- My project (3)/Assets/PlayerController.cs
+++ My project (3)/Assets/PlayerController.cs
@@ -5,8 +5,14 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5f;
+    public float jumpSpeed = 8f;
     private float direction = 0f;
     private Rigidbody2D player;
+
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask groundLayer;
+    private bool isTouchingGround;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +23,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        isTouchingGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
         direction = Input.GetAxis("Horizontal");
 
         if (direction > 0f)
@@ -31,5 +38,10 @@ public class PlayerController : MonoBehaviour
         {
             player.velocity = new Vector2(0, player.velocity.y);
         }
+
+        if (Input.GetButtonDown("Jump") && isTouchingGround)
+        {
+            player.velocity = new Vector2(player.velocity.x, jumpSpeed);
+        }
     }
-} 
+}
